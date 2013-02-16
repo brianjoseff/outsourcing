8d69467bca262c5b6b194df013fe79d54ac8ae6e
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2013-02-15 21:55:57 -0800

    ported

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def work
+  end
+
+  def expertise
+  end
+
+  def pricing
+  end
+
+  def contact
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outsourcing::Application.config.secret_token = 'aa335badb0b861c956932dba67f053550461e6ca2b7d5c05ec39b6dc2d5205bd16eddea98f06fdc532603cc8c26807df52c9f8d0c6153836520f07098d2a3200'
